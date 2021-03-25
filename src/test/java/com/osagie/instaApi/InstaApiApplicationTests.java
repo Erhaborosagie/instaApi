056a07d882e7abdd2573d30906d577353981cea5
Osagie Erhabor <erhaborosagie@gmail.com>
2021-03-25 08:33:24 +0100

    initial bootstrap

---
+++ src/test/java/com/osagie/instaApi/InstaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osagie.instaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
